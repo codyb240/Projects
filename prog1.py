1c3b7d2fdd6bba7a38846b8f5bb28dec0641b583
codyb240 <88124339+codyb240@users.noreply.github.com>
2022-11-04 23:31:12 -0500

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,34 @@
+#--------------------------------------------------------------------------------------------------------------------------
+# Programmer:Cody Bruehl
+# Program: 1
+# Date:7-4-20
+# Description: This program provides tutoring for addition, subtraction and multiplication.
+# The program generates a random arithmetic problem using numbers in the range 1-20.
+#--------------------------------------------------------------------------------------------------------------------------
+import random
+def randIntMax20():
+ # This function returns a random number between 1 and 20 inclusive
+#-------------------------------------------------------------------------------------------------------------------------
+
+#r = round(random.random()*10)
+# write a loop that repeats the following numProbs times
+ # using the function randIntMax20 generate two random numbers num1 and num2
+ # if num1 is less than num2, initialize num1 to 20
+ # using the standard random function (page 1, item II) generate a random number in the
+ # range 0 to 2 inclusive and save it in r
+ # if r is equal to 0, show an addition problem:
+ output num1 “ + ” num2 “ = ”
+ compute the correctAnswer
+ # if r is equal to 1, show a subtraction problem:
+ output num1 “ - ” num2 “ = ”
+ compute the correctAnswer
+ # if r is equal to 2, show a multiplication problem as follows:
+ output num1 “ x ” num2 “ = ”
+ compute the correctAnswer
+ # input the student’s answer and save it in studentAnswer (do not use any prompts).
+ # if studentAnswer matches the correctAnswer, output a message; else, output another
+ # message (see the sample session on page 1)
+ 
+
+
+#print(num1, ch , num2, ":", result)
